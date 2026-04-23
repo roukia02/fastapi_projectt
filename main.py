10f73705341733fa065606c17d8ff5be2a8461d3
roukia02 <rzaiter1@stu.vistula.edu.pl>
2026-04-24 00:06:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,17 +2,33 @@ from fastapi import FastAPI
 from fastapi.params import Body
 
 app = FastAPI()
+items = []
 users = []
+orders = []
 
 @app.get("/")
 def root():
+    return {"Message": f"{items}"}
+
+@app.post("/additem")
+def create_item(tomasz: dict = Body(...)):
+    items.append(tomasz)
+    return {f"Item created, NAME: {tomasz['name']}, QTY: {tomasz['quantity']}"}
+
+@app.get("/users")
+def get_users():
     return {"Message": f"{users}"}
 
 @app.post("/adduser")
-def create_item(tomasz: dict = Body(...)):
-    users.append(tomasz)
-    return {f"User created, NAME: {tomasz['name']}, AGE: {tomasz['age']}"}
+def add_user(user: dict = Body(...)):
+    users.append(user)
+    return {f"User created, NAME: {user['name']}, AGE: {user['age']}"}
 
-@app.get("/{number}")
-def get_user(number: int):
-    return {"Message": f"{users[number]}"}
+@app.get("/orders")
+def get_orders():
+    return {"Message": f"{orders}"}
+
+@app.post("/addorder")
+def add_order(order: dict = Body(...)):
+    orders.append(order)
+    return {f"Order created, PRODUCT: {order['product']}, QTY: {order['quantity']}"}
